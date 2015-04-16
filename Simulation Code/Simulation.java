89837d642d9376d2850f0603b3fb1abfae41bec5
Lawrence Moore <lsmoore938@gmail.com>
2015-04-16 00:01:11 -0400

    Made it so Simulation.java runs out the gate

--- Simulation Code/Simulation.java
+++ Simulation Code/Simulation.java
@@ -11,7 +11,7 @@ public class Simulation {
 	public static void main(String[] args) {
 		int totalTimesteps = 100, time = 0;
 		// Number of people the simulation should print stats for
-		int nPeopleStats = -1;
+		int nPeopleStats = 2;
 		// The interval used to print the statistics.
 		int statsInterval = HOURLY;
 		// Whether to report death or birth during updates
